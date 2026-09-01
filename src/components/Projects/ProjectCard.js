1f00cfa6ebd7463de808edeebd4a82d477976554
shruti881994 <57830840+shruti881994@users.noreply.github.com>
2026-09-01 15:00:28 +0530

    Update alt text for project card image

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -3,7 +3,7 @@ import bannerImg from "../../assets/aboutimg.jpg"
 function ProjectCard({title, mainContent}){
     return(
         <div className="p-3 md:p-6 flex flex-col w-72 bg-[#0c0e19] shadow-xl shadow-slate-900 rounded-2xl">
-            <img className="p-4" src={bannerImg} alt="project image"/>
+            <img className="p-4" src={bannerImg} alt="project card banner"/>
             <h3 className="px-4 text-xl md:text-2xl font-bold leading-normal">{title}</h3>
             <p className="px-4 text-sm md:text-md leading-tight py-2">{mainContent}</p>
             <div className="mt-2 p-2 md:p-4 flex gap-2 md:gap-4">
